cd003037b64758e24dadac528f50774faa581ac0
Tran Huu Thang <huuthangit.dn@gmail.com>
2019-10-19 11:29:15 +0700

    Update Location table

---
+++ Clinicia.Repositories/Migrations/20191019042737_AddMoreColumnsToLocationTable.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Clinicia.Repositories.Migrations
+{
+    public partial class AddMoreColumnsToLocationTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Locations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Locations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Locations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Zip",
+                table: "Locations",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "Zip",
+                table: "Locations");
+        }
+    }
+}
--- Clinicia.Repositories/Schemas/DbLocation.cs
+++ Clinicia.Repositories/Schemas/DbLocation.cs
@@ -14,5 +14,13 @@ namespace Clinicia.Repositories.Schemas
         public double Longitude { get; set; }
 
         public double Latitude { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public string Zip { get; set; }
     }
 }
